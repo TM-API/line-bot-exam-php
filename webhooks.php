f2a87799934337573d18b55957fae838f2d7e0ac
TM-API <60170620+TM-API@users.noreply.github.com>
2020-01-23 09:19:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'T3LNz+eC1g7X7G6EZo2iPsjlOHXAv6Ri1jbxh+T26c+pF55vxDOrR7p00lDRwJfNCrFrojXUwBrMiOMhH6A6V8hWAaCs+1QnI0WYrNy9rqvjvVvq58JLdiYfwAW1wjAXpx1zFqTzHdyfDiQja8okOwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
